b70b2db958a47a1855f2ae195d3205ea0a3e30ed
Marco Antonio Sugamele <sugamele.marco@gmail.com>
2014-06-20 09:59:31 -0300

    Fixed order install php.ini

--- files/scripts/php/php.sh
+++ files/scripts/php/php.sh
@@ -8,9 +8,3 @@ sudo apt-get -y update
 
 sudo apt-get -y install php5 php5-dev php5-cli php-pear
 
-
-sudo mv /etc/php5/cli/php.ini /etc/php5/cli/php.ini.old
-sudo cp /vagrant/files/php/php.ini /etc/php5/cli/
-
-sudo mv /etc/php5/fpm/php.ini /etc/php5/fpm/php.ini.old
-sudo cp /vagrant/files/php/php.ini /etc/php5/fpm/
--- files/scripts/php/php_libs.sh
+++ files/scripts/php/php_libs.sh
@@ -7,3 +7,6 @@ sudo apt-get -y install php5-curl php5-json php5-gd php5-fpm
 
 sudo mv /etc/php5/cli/php.ini /etc/php5/cli/php.ini.old
 sudo cp /vagrant/files/php/php.ini /etc/php5/cli/ 
+
+sudo mv /etc/php5/fpm/php.ini /etc/php5/fpm/php.ini.old
+sudo cp /vagrant/files/php/php.ini /etc/php5/fpm/
