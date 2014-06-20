fcceed2837a77f03a5878a9e14bccfdd518dddd7
Marco Antonio Sugamele <sugamele.marco@gmail.com>
2014-06-20 10:01:15 -0300

    Fixed Locales

--- files/scripts/init.sh
+++ files/scripts/init.sh
@@ -5,5 +5,5 @@ sudo apt-get -y install build-essential
 sudo apt-get -y install curl
 apt-get -y install vim
 
-sudo apt-get install --reinstall locales && sudo dpkg-reconfigure locales
-sudo locale-gen en_US.UTF-8
+sudo apt-get install --reinstall locales
+sudo locale-gen pt_BR.UTF-8
