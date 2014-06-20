f9aad4a8f6629e3e6346d18252c571211fcac4e7
sugamele <sugamele.marco@gmail.com>
2014-06-19 23:29:32 -0300

    released external access to mongodb

--- files/scripts/php/lib-mongo.sh
+++ files/scripts/php/lib-mongo.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 no '' | sudo pecl install mongo
+
+sudo mv /etc/mongod.conf /etc/mongod.conf.old
+sudo cp /vagrant/files/mongo/mongod.conf /etc/
--- files/scripts/ruby/bootstrap_app.sh
+++ files/scripts/ruby/bootstrap_app.sh
@@ -6,4 +6,7 @@ cd /var/www/app
 bundle install
 bundle update
 
+sudo mv /etc/mongod.conf /etc/mongod.conf.old
+sudo cp /vagrant/files/mongo/mongod.conf /etc/
+
 sudo service nginx start
